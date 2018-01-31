6928ebfeb28853c4b7db6f3ec70abf63a9b933b0
Adam Kalt <adam.kalt.1@gmail.com>
2018-01-31 04:55:37 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ return Object.assign({}, object, { [key]: value });
 function destructivelyUpdateObjectWithKeyAndValue (object, key, value){
 object[key] = value;
 
-return object
+return object;
 }
 
 function deleteFromObjectByKey (object, key){
